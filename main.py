724606aae20be9421f1c95e509775349f8e0c059
denzow <denzow@gmail.com>
2018-01-07 12:45:52 +0900

    [add] history class

--- main.py
+++ main.py
@@ -11,6 +11,35 @@ SECRET = os.environ.get('ZAIF_SECRET') or 'YOUR_SECRET'
 TARGET_CURRENCY_PAIR = 'btc_jpy'
 
 
+class TradeHistory:
+
+    def __init__(self):
+        self.history = []
+
+    def add_history(self, trade_data):
+        """
+
+        :param TradeData trade_data:
+        :return:
+        """
+        self.history.append(trade_data)
+        if len(self.history) > 100:
+            self.history = self.history[-100:]
+
+    def is_up(self, other_trade_data):
+        """
+
+        :param TradeData other_trade_data:
+        :return:
+        """
+        if not self.history:
+            return False
+
+        last_history = self.history[-1]
+
+        return last_history.get_whole_avg() < other_trade_data.get_whole_avg()
+
+
 class TradeData:
 
     def __init__(self, time_str, last_price, trades):
@@ -50,6 +79,9 @@ class TradeData:
             'avg_price': avg_price
         }
 
+    def get_whole_avg(self):
+        total_price = self.bid_avg_price * self.bid_amount + self.ask_avg_price * self.ask_amount
+        return round(total_price / (self.ask_amount + self.bid_amount), 3)
 
 # TODO
 def trade():
@@ -66,18 +98,15 @@ def main():
     :return:
     """
     zaif_stream = ZaifPublicStreamApi()
-    history = []
+    trade_history = TradeHistory()
     # StreamAPIはジェネレータを戻しているのでこれでずっといける
     for stream in zaif_stream.execute(currency_pair=TARGET_CURRENCY_PAIR):
         time_str = stream['timestamp']
         last_prince = stream['last_price']['price']
         trades = stream['trades']
         trade_data = TradeData(time_str, last_prince, trades)
-        print(time_str, last_prince, trade_data.ask_avg_price, trade_data.bid_avg_price)
-        history.append(trade_data)
-
-        if len(history) > 100:
-            history = history[-100:]
+        print(trade_history.is_up(trade_data), time_str, last_prince, trade_data.ask_avg_price, trade_data.bid_avg_price)
+        trade_history.add_history(trade_data)
 
 
 if __name__ == '__main__':
