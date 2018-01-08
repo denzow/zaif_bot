e3f5575655fad384034f47068e89b7c0ee08cffe
denzow <denzow@gmail.com>
2018-01-08 21:37:11 +0900

    [modify] comments

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 # coding: utf-8
-import os
+import datetime
 from zaifapi import ZaifPublicStreamApi
 
-
-#KEY = os.environ.get('ZAIF_KEY') or 'YOUR_KEY'
-#SECRET = os.environ.get('ZAIF_SECRET') or 'YOUR_SECRET'
 TARGET_CURRENCY_PAIR = 'btc_jpy'
 
 
 class TradeHistory:
 
-    def __init__(self):
+    def __init__(self, history_limit_count=1000):
         self.trade_history = []
+        self.history_limit_count = history_limit_count
 
     def add_history(self, trade_data):
         """
@@ -20,34 +18,12 @@ class TradeHistory:
         :return:
         """
         self.trade_history.append(trade_data)
-        if len(self.trade_history) > 100:
-            self.trade_history = self.trade_history[-100:]
-
-    def up_or_down(self, other_trade_data, check_type='trade'):
-        """
-
-        :param TradeData other_trade_data:
-        :param str check_type:
-        :return:
-        """
-        if not self.trade_history:
-            return 'N/A'
-        difference = 0
-        if check_type == 'trade':
-            difference = self.get_trade_difference(other_trade_data)
-        elif check_type == 'depth':
-            difference = self.get_depth_difference(other_trade_data)
-
-        if difference == 0:
-            return 'STY'
-        if difference > 0:
-            return 'UP.'
-        if difference < 0:
-            return 'DWN'
+        if len(self.trade_history) > self.history_limit_count:
+            self.trade_history = self.trade_history[-self.history_limit_count:]
 
     def get_trade_difference(self, other_trade_data):
         """
-
+        トレード情報の平均価格の変化量を取得
         :param TradeData other_trade_data:
         :return:
         """
@@ -59,7 +35,7 @@ class TradeHistory:
 
     def get_depth_difference(self, other_trade_data):
         """
-
+        板情報の平均価格の変化量を取得
         :param TradeData other_trade_data:
         :return:
         """
@@ -71,7 +47,7 @@ class TradeHistory:
 
     def get_last_price_difference(self, other_trade_data):
         """
-
+        終値の変化量を取得
         :param TradeData other_trade_data:
         :return:
         """
@@ -83,25 +59,36 @@ class TradeHistory:
 
 
 class TradeData:
+    """
+    ある時点での取引情報
+    """
 
-    def __init__(self, time_str, last_price, trades, asks, bids):
-        self.time_str = time_str
+    def __init__(self, trade_time, last_price, trades, asks, bids):
+        """
+
+        :param trade_time: 情報時刻
+        :param last_price: 終値
+        :param list trades: 取引実績
+        :param list[float float] asks: ask一覧
+        :param list[float float] bids: bid一覧
+        """
+        self.trade_time = trade_time
         self.last_price = last_price
         self.trades = trades
         self.asks = asks
         self.bids = bids
 
         bid_data = self._get_calculated_trades('bid')
-        self.bid_amount = bid_data['amount']
-        self.bid_max_price = bid_data['max_price']
-        self.bid_min_price = bid_data['min_price']
-        self.bid_avg_price = bid_data['avg_price']
+        self.trade_bid_amount = bid_data['amount']
+        self.trade_bid_max = bid_data['max_price']
+        self.trade_bid_min = bid_data['min_price']
+        self.trade_bid_avg = bid_data['avg_price']
 
         ask_data = self._get_calculated_trades('ask')
-        self.ask_amount = ask_data['amount']
-        self.ask_max_price = ask_data['max_price']
-        self.ask_min_price = ask_data['min_price']
-        self.ask_avg_price = ask_data['avg_price']
+        self.trade_ask_amount = ask_data['amount']
+        self.trade_ask_max = ask_data['max_price']
+        self.trade_ask_min = ask_data['min_price']
+        self.trade_ask_avg = ask_data['avg_price']
 
         self.depth_ask_amount, self.depth_ask_avg = self._get_calculated_depth('ask')
         self.depth_bid_amount, self.depth_bid_avg = self._get_calculated_depth('bid')
@@ -116,9 +103,16 @@ class TradeData:
 
         total = sum([tr['price'] * tr['amount'] for tr in target_list])
         amount = sum([tr['amount'] for tr in target_list])
-        avg_price = round(total / amount, 3)
-        max_price = max([tr['amount'] for tr in target_list])
-        min_price = min([tr['amount'] for tr in target_list])
+        # 取引量が0の場合は終値をそのまま採用する
+        if amount:
+            avg_price = round(total / amount, 3)
+            max_price = max([tr['amount'] for tr in target_list])
+            min_price = min([tr['amount'] for tr in target_list])
+        else:
+            avg_price = self.last_price
+            max_price = self.last_price
+            min_price = self.last_price
+
         return {
             'amount': amount,
             'max_price': max_price,
@@ -127,6 +121,11 @@ class TradeData:
         }
 
     def _get_calculated_depth(self, board_type):
+        """
+        板情報をまとめて、平均価格や取引量を求める
+        :param board_type:
+        :return:
+        """
         target_data = None
         if board_type == 'ask':
             target_data = self.asks
@@ -137,10 +136,18 @@ class TradeData:
         return trade_amount, round(trade_total/trade_amount, 3)
 
     def get_whole_trade_avg(self):
-        total_price = self.bid_avg_price * self.bid_amount + self.ask_avg_price * self.ask_amount
-        return round(total_price / (self.ask_amount + self.bid_amount), 3)
+        """
+        全トレードの平均価格
+        :return:
+        """
+        total_price = self.trade_bid_avg * self.trade_bid_amount + self.trade_ask_avg * self.trade_ask_amount
+        return round(total_price / (self.trade_ask_amount + self.trade_bid_amount), 3)
 
     def get_whole_depth_avg(self):
+        """
+        全板の平均価格
+        :return:
+        """
         total_price = self.depth_ask_avg * self.depth_ask_amount + self.depth_bid_avg * self.depth_bid_amount
         return round(total_price / (self.depth_ask_amount + self.depth_bid_amount), 3)
 
@@ -156,20 +163,21 @@ def main():
         # StreamAPIはジェネレータを戻しているのでこれでずっといける
         for stream in zaif_stream.execute(currency_pair=TARGET_CURRENCY_PAIR):
             time_str = stream['timestamp']
-            last_prince = stream['last_price']['price']
+            trade_time = datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S.%f')
+            last_price = stream['last_price']['price']
             trades = stream['trades']
             asks = stream['asks']
             bids = stream['bids']
 
-            trade_data = TradeData(time_str, last_prince, trades, asks, bids)
+            trade_data = TradeData(trade_time, last_price, trades, asks, bids)
             print(
                 time_str,
                 trade_history.get_trade_difference(trade_data),
                 trade_history.get_depth_difference(trade_data),
                 trade_history.get_last_price_difference(trade_data),
-                last_prince,
-                trade_data.ask_avg_price,
-                trade_data.bid_avg_price
+                last_price,
+                trade_data.trade_ask_avg,
+                trade_data.trade_bid_avg
             )
             trade_history.add_history(trade_data)
     except KeyboardInterrupt:
